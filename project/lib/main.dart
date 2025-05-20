ea465290983041d6492462790e4df1338c4084d1
f04k04369 <curb369@gmail.com>
2025-05-20 18:13:29 +0900

    #8 テキストフィールド演習

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -3,76 +3,103 @@ import 'package:project/banana_counter.dart';
 
 void main() {
 
-  xxxx() {
-    debugPrint('これから通信を始めます');
+  final controller = TextEditingController();
+
+  final textField = TextField(
+    controller: controller,
+
+    decoration: InputDecoration(
+      border: OutlineInputBorder(),
+      labelText: 'あなたのなまえ',
+      hintText: 'カタカナで入力してください',
+      errorText: '名前が長すぎます',
+    ),
+  );
+
+  zzzz() {
+    debugPrint( controller.text );
   }
 
-  final button = ElevatedButton(
-    onPressed: xxxx, 
-    child: Text('おしてみて') ,
+  final button = ElevatedButton(onPressed: zzzz, child: const Text('ボタン'));
 
-    style: ElevatedButton.styleFrom(
-      backgroundColor: Colors.green,
-    ),
-    );
+  // xxxx() {
+  //   debugPrint('これから通信を始めます');
+  // }
 
-  final bananaCounter = BananaCounter(
-    number: 888,
+  // final button = ElevatedButton(
+  //   onPressed: xxxx, 
+  //   child: Text('おしてみて') ,
 
-  );
+  //   style: ElevatedButton.styleFrom(
+  //     backgroundColor: Colors.green,
+  //   ),
+  //   );
 
-  final col = Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: const [
-      Text('レモン'),
-      Text('リンゴ'),
-      Text('ブドウ'),
-    ],
-  );
+  // final bananaCounter = BananaCounter(
+  //   number: 888,
 
-  final img = Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Image.asset(
-        'assets/images/saku.png'
-        ),
-      Image.network(
-        'https://flutter-image-network.web.app/inu.jpeg'
-      )
-    ],
-  );
+  // );
 
-  final con = Container(
-    color: Colors.deepOrange,
-    width: 500,
-    height: 400,
-    child: col,
-    alignment: Alignment.centerLeft,
-    padding: EdgeInsets.fromLTRB(20,10,0,100),
-    margin: EdgeInsets.all(20),
-  );
+  // final col = Column(
+  //   mainAxisAlignment: MainAxisAlignment.center,
+  //   crossAxisAlignment: CrossAxisAlignment.center,
+  //   children: const [
+  //     Text('レモン'),
+  //     Text('リンゴ'),
+  //     Text('ブドウ'),
+  //   ],
+  // );
 
-  final rowItems = Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      col, col, col
-    ],
-  );
+  // final img = Column(
+  //   mainAxisAlignment: MainAxisAlignment.center,
+  //   crossAxisAlignment: CrossAxisAlignment.center,
+  //   children: [
+  //     Image.asset(
+  //       'assets/images/saku.png'
+  //       ),
+  //     Image.network(
+  //       'https://flutter-image-network.web.app/inu.jpeg'
+  //     )
+  //   ],
+  // );
 
-  final colItems = Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      col, col, col
-    ],
-  );
+  // final con = Container(
+  //   color: Colors.deepOrange,
+  //   width: 500,
+  //   height: 400,
+  //   child: col,
+  //   alignment: Alignment.centerLeft,
+  //   padding: EdgeInsets.fromLTRB(20,10,0,100),
+  //   margin: EdgeInsets.all(20),
+  // );
+
+  // final rowItems = Row(
+  //   mainAxisSize: MainAxisSize.min,
+  //   children: [
+  //     col, col, col
+  //   ],
+  // );
+
+  // final colItems = Column(
+  //   mainAxisSize: MainAxisSize.min,
+  //   children: [
+  //     col, col, col
+  //   ],
+  // );
 
   final a = MaterialApp(
     home: Scaffold(
       body: Center(
-        child: 
-        button,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 300,
+              child: textField,
+            ),
+            button
+          ],
+        ),
       ),
     ),
   );
