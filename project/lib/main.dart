3bf904ea23a507eb11fbc7af367e5588d92d1247
f04k04369 <curb369@gmail.com>
2025-05-24 14:51:21 +0900

    #17 freezedについて

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,110 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:percent_indicator/flutter_percent_indicator.dart';
+import 'package:project/abc_list.dart';
 
+// 自分が作った fish.dart ファイルを読み込む
+import 'package:project/fish.dart';
 
-// アプリ全体
-main() {
-  // アプリ
-  const app = MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Root(),
-  );
+// 自分が作った abc_list.dart ファイルを読み込む
+
+// メイン関数
+void main() {
+  const app = MaterialApp(home: Home());
   const scope = ProviderScope(child: app);
-
   runApp(scope);
 }
 
-// どのくらい進んだかを表す　パーセント
-final percentProvider = StateProvider(
-  (ref) {
-// 最初はどれも選ばれていないのでnull
-    return 0.00;
-  },
-);
+// 魚データの状態管理
+final fishProvider = StateProvider((ref) {
+  return const Fish(
+    name: 'マグロ',
+    size: 200,
+    price: 300,
+  );
+});
 
+// ABCリストの状態管理
+final abcListProvider = StateProvider((ref) {
+  return AbcList([
+    'A',
+    'B',
+    'C',
+  ]);
+});
 
-// 画面全体を定義
-class Root extends ConsumerWidget {
-  const Root({super.key});
+// ホーム画面
+class Home extends ConsumerWidget {
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref)  {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 魚データ
+    final fish = ref.watch(fishProvider);
+    // ABCリスト
+    final abcList = ref.watch(abcListProvider);
 
-    // パーセント
-    final percent = ref.watch(percentProvider);
-
-    // 丸形のインジケーター
-    final circular = CircularPercentIndicator(
-      percent: percent,
-      backgroundColor: Colors.yellow,
-      progressColor: Colors.green,
-      radius: 60,
-      lineWidth: 20,
-      center: Text('${percent * 100}%'),
-      animation: true,
-      animationDuration: 200,
-      animateFromLastPercent: true,
+    // 名前テキスト
+    final nameText = Text(
+      '名前: ${fish.name}',
     );
 
-    // 棒型のインジケーター
-    final linear = LinearPercentIndicator(
-      percent: percent,
-      backgroundColor: Colors.black,
-      progressColor: Colors.amber,
-      alignment: MainAxisAlignment.center,
-      lineHeight: 20,
-      width: 300,      
-      animation: true,
-      animationDuration: 200,
-      animateFromLastPercent: true,
-
+    // 大きさテキスト
+    final sizeText = Text(
+      '大きさ: ${fish.size} cm',
     );
 
-    final col  = Column(
+    // 値段テキスト
+    final priceText = Text(
+      '値段: ${fish.price} 万円',
+    );
+
+    // ABCリストテキスト
+    final abcListText = Text(
+      'ABCリスト: $abcList',
+    );
+
+    // ボタン
+    final button = ElevatedButton(
+      onPressed: () => onPressed(ref),
+      child: const Text('変更する'),
+    );
+
+    // 縦に並べるカラム
+    final column = Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      spacing: 40,
       children: [
-        circular,
-        linear,
+        nameText,
+        sizeText,
+        priceText,
+        abcListText,
+        button,
       ],
     );
 
-    // ボタンを押したときの関数
-    void onPressed(WidgetRef ref) async{
-      // 1秒間待つ
-      await Future.delayed(const Duration(seconds: 1));
-      ref.read(percentProvider.notifier).state = 0.20;
-      // １秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      ref.read(percentProvider.notifier).state = 0.40;      
-      // 1秒間待つ
-      await Future.delayed(const Duration(seconds: 1));
-      ref.read(percentProvider.notifier).state = 0.60;
-      // １秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      ref.read(percentProvider.notifier).state = 0.80;      
-      // １秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      ref.read(percentProvider.notifier).state = 1.00;
-    }
- 
-    // 画面を返す
+    // 画面の真ん中にカラムを置く
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            col,
-            ElevatedButton(
-              onPressed: () => {
-                onPressed(ref),
-              }, 
-              child: const Text('スタート'),
-            ),
-          ],
-        ),
+        child: column,
       ),
     );
+  }
+
+  // ボタンを押したときの関数
+  void onPressed(WidgetRef ref) {
+    // 今画面に出ている魚
+    final fish = ref.read(fishProvider);
+
+    // 入れ物ごと変えた 新しい魚
+    final newFish = fish.copyWith(
+      // 値段は 500 にする
+      price: 300,
+    );
+
+    // 画面を変更する
+    ref.read(fishProvider.notifier).state = newFish;
+
+    // --- --- --- ---
+
+    // 今画面に出ているABCリスト
+    final abcList = ref.read(abcListProvider);
+
+    // 入れ物ごと変えた 新しいABCリスト
+    final newAbcList = abcList.copyWith(
+      // D を追加する
+      values: abcList.values + ['D'],
+    );
+
+    // 画面を変更する
+    ref.read(abcListProvider.notifier).state = newAbcList;
   }
 }
